77d979fba8fabc56beb86165a82a69b68ea4119e
Ali <alifeyza4017@gmail.com>
2022-09-17 15:29:43 +0300

    added

--- main.js
+++ main.js
@@ -10,20 +10,34 @@ const pri =document.querySelector("#pri")
 const mat =document.querySelector("#mat")
 const sum =document.querySelector("#sum")
 const interest = document.querySelector("#int")
+const amo = document.querySelector("#amo")
+const lo =document.querySelector("#lo")
+
 
 
 
 calc.addEventListener("click", function() {
-    if(!maturity.value || !price.value) {
+
+    if(!maturity.value || !price.value ) {
         alert("Please enter maturity and amount information")
     }else {
         container1.classList.remove("hidden")
+        
         pri.textContent= price.value;
         mat.textContent = maturity.value
         let a = interest.textContent * Math.pow((1 + interest.textContent), mat.textContent)
         let b = Math.pow(1 + interest.textContent, mat.textContent) -1
-        let c = pri.textContent * (a / b)
-        
+        let c = (pri.textContent * (a / b)).toFixed(2)
+        sum.textContent = c
+        amo.textContent = c / mat.textContent;
+
+       
         
     }
+})
+
+clean.addEventListener("click", function () {
+    maturity.value = ""
+    price.value = ""
+    container1.classList.add("hidden")
 })
